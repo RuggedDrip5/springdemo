02286dbd0dd4f607ca819b41a6db61a6882788ef
RuggedDrip5 <afinoshka@gmail.com>
2025-05-06 01:09:10 +0300

    Главный класс приложения

--- src/main/java/ru/otus/springaopdemo/SpringAopDemoApplication.java
+++ src/main/java/ru/otus/springaopdemo/SpringAopDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringAopDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAopDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopDemoApplication.class, args);
+    }
 }
